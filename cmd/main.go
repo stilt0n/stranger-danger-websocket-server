810323b6cf28b3c98833352885c89f76500195f9
stilt0n <mattias.leino@arts.cornish.edu>
2024-01-18 21:54:48 -0500

    create router

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"stranger-danger/db"
+	"stranger-danger/internal/user"
+	"stranger-danger/router"
 
 	// Need to include postgres driver for database to work
 	// this isn't used so we need to alias it w/ _
@@ -10,11 +12,18 @@ import (
 )
 
 func main() {
-	database, err := db.NewDatabase()
+	dbConn, err := db.NewDatabase()
 	if err != nil {
 		log.Fatalf("Could not connect to database: %s", err)
 		return
 	}
-	defer database.Close()
+	defer dbConn.Close()
 	log.Printf("Successfully connected to the database!")
+
+	userRepo := user.NewRepository(dbConn.GetDB())
+	userService := user.NewService(userRepo)
+	userHandler := user.NewHandler(userService)
+
+	router.InitRouter(userHandler)
+	router.Start("0.0.0.0:8080")
 }
